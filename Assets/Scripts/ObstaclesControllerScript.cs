c056617daae7899aa3f047192187871a16741485
RudolfsSuklais <ruudisrudolfs@gmail.com>
2025-10-13 13:22:55 +0300

    updated

--- Assets/Scripts/ObstaclesControllerScript.cs
+++ Assets/Scripts/ObstaclesControllerScript.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,23 +6,22 @@ public class ObstaclesControllerScript : MonoBehaviour
 {
     [HideInInspector]
     public float speed = 1f;
+    public float fadeDuration = 1.5f;
     public float waveAmplitude = 25f;
     public float waveFrequency = 1f;
-    public float fadeDuration = 1.5f;
     private ObjectScript objectScript;
-    private ScreenBoundriesScript screenBoundriesScript;
+    private ScreenBoundriesScript scrreenBoundriesScript;
     private CanvasGroup canvasGroup;
     private RectTransform rectTransform;
     private bool isFadingOut = false;
+    private bool isExploading = false;
     private Image image;
     private Color originalColor;
-
-
 
     void Start()
     {
         canvasGroup = GetComponent<CanvasGroup>();
-        if(canvasGroup == null )
+        if (canvasGroup == null)
         {
             canvasGroup = gameObject.AddComponent<CanvasGroup>();
         }
@@ -31,113 +30,121 @@ public class ObstaclesControllerScript : MonoBehaviour
 
         image = GetComponent<Image>();
         originalColor = image.color;
-
-        objectScript = Object.FindFirstObjectByType<ObjectScript>();
-        screenBoundriesScript = Object.FindFirstObjectByType<ScreenBoundriesScript>();
+        objectScript = FindFirstObjectByType<ObjectScript>();
+        scrreenBoundriesScript = FindFirstObjectByType<ScreenBoundriesScript>();
         StartCoroutine(FadeIn());
-
     }
 
-
+    // Update is called once per frame
     void Update()
     {
         float waveOffset = Mathf.Sin(Time.time * waveFrequency) * waveAmplitude;
         rectTransform.anchoredPosition += new Vector2(-speed * Time.deltaTime, waveOffset * Time.deltaTime);
-
-
-       if (speed > 0 && transform.position.x < (screenBoundriesScript.minX + 80) && !isFadingOut)
+        // <-
+        if (speed > 0 && transform.position.x < (scrreenBoundriesScript.minX + 80) && !isFadingOut)
         {
-            isFadingOut = true;
             StartCoroutine(FadeOutAndDestroy());
+            isFadingOut = true;
         }
 
-
-        if (speed < 0 && transform.position.x > (screenBoundriesScript.maxX - 80) && !isFadingOut)
+        // ->
+        if (speed < 0 && transform.position.x > (scrreenBoundriesScript.maxX - 80) && !isFadingOut)
         {
-            isFadingOut = true;
             StartCoroutine(FadeOutAndDestroy());
+            isFadingOut = true;
         }
 
-        if(ObjectScript.drag && !isFadingOut &&
+        if (CompareTag("Bomb") && !isExploading &&
             RectTransformUtility.RectangleContainsScreenPoint(
                 rectTransform, Input.mousePosition, Camera.main))
         {
-            Debug.Log("Obstacle hit by drag");
-            if(ObjectScript.lastDragged != null)
+            Debug.Log("The cursor collided with a bomb! (without car)");
+            TriggerExplosion();
+
+        }
+
+        // Caurskatīt no šejienes
+
+
+        if (ObjectScript.drag && !isFadingOut &&
+            RectTransformUtility.RectangleContainsScreenPoint(rectTransform, Input.mousePosition, Camera.main))
+        {
+            Debug.Log("The cursor collided with a flying object!");
+
+            if (ObjectScript.lastDragged != null)
             {
                 StartCoroutine(ShrinkAndDestroy(ObjectScript.lastDragged, 0.5f));
                 ObjectScript.lastDragged = null;
                 ObjectScript.drag = false;
             }
 
-            StartCoroutine(FadeOutAndDestroy());
-            isFadingOut = true;
+            StartToDestroy();
+        }
+    }
 
-            image.color = Color.cyan;
-            StartCoroutine(RecoverColor());
+    public void TriggerExplosion()
+    {
+        isExploading = true;
+        objectScript.effects.PlayOneShot(objectScript.audioCli[6], 5f);
 
-            StartCoroutine(Vibrate());
+        if (TryGetComponent<Animator>(out Animator animator))
+        {
+            animator.SetBool("explode", true);
+        }
 
-            if(objectScript.effects != null && objectScript.audioCli != null)
+        image.color = Color.red;
+        StartCoroutine(RecoverColor(0.4f));
+
+        StartCoroutine(Vibrate());
+        StartCoroutine(WaitBeforeExpload());
+    }
+
+    IEnumerator WaitBeforeExpload()
+    {
+        float radius = 0f;
+        if (TryGetComponent<CircleCollider2D>(out CircleCollider2D circleCollider))
+        {
+            radius = circleCollider.radius * transform.lossyScale.x;
+        }
+        ExploadAndDestroy(radius);
+        yield return new WaitForSeconds(1.8f);
+        ExploadAndDestroy(radius);
+        Destroy(gameObject);
+    }
+
+    void ExploadAndDestroy(float radius)
+    {
+        Collider2D[] hitColliders = Physics2D.OverlapCircleAll(transform.position, radius);
+
+        foreach (var hitCollider in hitColliders)
+        {
+            if (hitCollider != null && hitCollider.gameObject != gameObject)
             {
-                objectScript.effects.PlayOneShot(objectScript.audioCli[5]);
+                ObstaclesControllerScript obj =
+                    hitCollider.gameObject.GetComponent<ObstaclesControllerScript>();
+
+                if (obj != null && !obj.isExploading)
+                {
+                    obj.StartToDestroy();
+                }
             }
         }
     }
 
-    IEnumerator FadeIn()
+    public void StartToDestroy()
     {
-        float a = 0f;
-        while(a < fadeDuration)
+        if (!isFadingOut)
         {
-            a += Time.deltaTime;
-            canvasGroup.alpha = Mathf.Lerp(0f, 1f, a / fadeDuration);
-            yield return null;
+            StartCoroutine(FadeOutAndDestroy());
+            isFadingOut = true;
+
+            image.color = Color.cyan;
+            StartCoroutine(RecoverColor(0.5f));
+
+            objectScript.effects.PlayOneShot(objectScript.audioCli[5]);
+
+            StartCoroutine(Vibrate());
         }
-
-        canvasGroup.alpha = 1f;
-    }
-
-    IEnumerator FadeOutAndDestroy()
-    {
-        float a = 0f;
-        float startAlpha = canvasGroup.alpha;
-
-         
-        
-        while (a < fadeDuration)
-        {
-            a += Time.deltaTime;
-            canvasGroup.alpha = Mathf.Lerp(0f, 1f, a / fadeDuration);
-            yield return null;
-        }
-
-        canvasGroup.alpha = 1f;
-        Destroy(gameObject);
-    }
-
-    IEnumerator ShrinkAndDestroy(GameObject target, float duration)
-    {
-        Vector3 originalScale = target.transform.localScale;
-        Quaternion originalRotation = target.transform.rotation;
-        float t = 0f;
-
-        while (t < duration)
-        {
-            t += Time.deltaTime;
-            target.transform.localScale = Vector3.Lerp(originalScale, Vector3.zero, t / duration);
-            float angle = Mathf.Lerp(0, 360, t / duration);
-            target.transform.rotation = Quaternion.Euler(0, 0, angle);
-
-            yield return null;
-        }
-
-        Destroy(target);
-    }
-    IEnumerator RecoverColor()
-    {
-        yield return new WaitForSeconds(0.5f);
-        image.color = originalColor;
     }
 
     IEnumerator Vibrate()
@@ -147,11 +154,66 @@ public class ObstaclesControllerScript : MonoBehaviour
         float elpased = 0f;
         float intensity = 5f;
 
-        while(elpased < duration)
+        while (elpased < duration)
         {
-            rectTransform.anchoredPosition = originalPosition + Random.insideUnitCircle * intensity;
+            rectTransform.anchoredPosition =
+                originalPosition + Random.insideUnitCircle * intensity;
             elpased += Time.deltaTime;
             yield return null;
         }
+        rectTransform.anchoredPosition = originalPosition;
     }
+
+    IEnumerator FadeIn()
+    {
+        float t = 0f;
+        while (t < fadeDuration)
+        {
+            t += Time.deltaTime;
+            canvasGroup.alpha = Mathf.Lerp(0f, 1f, t / fadeDuration);
+            yield return null;
+        }
+        canvasGroup.alpha = 1f;
+    }
+
+    IEnumerator FadeOutAndDestroy()
+    {
+        float t = 0f;
+        float startAlpha = canvasGroup.alpha;
+
+        while (t < fadeDuration)
+        {
+            t += Time.deltaTime;
+            canvasGroup.alpha = Mathf.Lerp(startAlpha, 0f, t / fadeDuration);
+            yield return null;
+        }
+        canvasGroup.alpha = 0f;
+        Destroy(gameObject);
+    }
+
+    IEnumerator ShrinkAndDestroy(GameObject target, float duration)
+    {
+        Vector3 orginalScale = target.transform.localScale;
+        Quaternion orginalRotation = target.transform.rotation;
+        float t = 0f;
+
+        while (t < duration)
+        {
+            t += Time.deltaTime;
+            target.transform.localScale = Vector3.Lerp(orginalScale, Vector3.zero, t / duration);
+            float angle = Mathf.Lerp(0f, 360f, t / duration);
+            target.transform.rotation = Quaternion.Euler(0f, 0f, angle);
+
+            yield return null;
+        }
+        Destroy(target);
+    }
+
+    IEnumerator RecoverColor(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        image.color = originalColor;
+    }
+
+
 }
